70b5243ada454ffb7bbe4464f489102995052c5f
Ziv <ziv_harpaz@bmc.com>
2025-01-30 14:35:18 +0200

    initial

---
+++ src/main/java/com/bmc/ctm/App.java
@@ -0,0 +1,13 @@
+package com.bmc.ctm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
