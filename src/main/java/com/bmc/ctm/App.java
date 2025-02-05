926549afa31b77b18c8d3ab44771c06a35f36f5f
Ziv <ziv_harpaz@bmc.com>
2025-02-05 11:15:04 +0200

    Using lib

--- src/main/java/com/bmc/ctm/App.java
+++ src/main/java/com/bmc/ctm/App.java
@@ -4,10 +4,10 @@ package com.bmc.ctm;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.printf( "Hello World!", Gen.gen_message() );
     }
 }
